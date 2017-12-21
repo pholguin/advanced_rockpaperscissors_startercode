9ef191770be3828d65054560960f99476f85c09f
Pamela Holguin <pholguin.student@hsfi.us>
2017-12-21 23:16:54 +0000

    finish last revisions

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 //Class 1:
-        // Fork and clone the repo and set up your project workspace and link files
-        // Write HTML for your project
+// Fork and clone the repo and set up your project workspace and link files
+// Write HTML for your project
 //Class 2:  
-        // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
-        // Practice using console.log() and debugger to debug your code
-        // Create your document ready function. 
-        // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
-        // Display the user choice to the output screen
+// Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
+// Practice using console.log() and debugger to debug your code
+// Create your document ready function. 
+// Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
+// Display the user choice to the output screen
 //Class 3:  
-        // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
-        // Display the computer choice to the output screen
-        // Compare the user choice and the computer choice to determine who won. 
-        // Display the user winner to the output screen 
-        // Clear the input box for the next choice
+// Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
+// Display the computer choice to the output screen
+// Compare the user choice and the computer choice to determine who won. 
+// Display the user winner to the output screen 
+// Clear the input box for the next choice
 
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
@@ -34,13 +34,47 @@
 
 // DOCUMENT READY FUNCTION
 $(document).ready(function() {
-        $("#shoot").click(function() {
-                var choices= ["rock", "paper", "scissors"];
-                var computerchoiceIndex= Math.random()*choices.length;
-                        console.log(choices[Math.floor(computerchoiceIndex)]);
-                $(".computer").text(choices[Math.floor(computerchoiceIndex)]);
-                $(".user").text(choices[Math.floor(computerchoiceIndex)]);
-        });
+                        $("#shoot").click(function() {
+                                var choices = ["rock", "paper", "scissors"];
+                                var computerchoiceIndex = Math.random() * choices.length;
+                                console.log(choices[Math.floor(computerchoiceIndex)]);
+
+                                var computerChoice = choices[Math.floor(computerchoiceIndex)];
+                                var userChoice = $('#userInput').val();
+                                $(".computer").text(computerChoice);
+                                $(".user").text(userChoice);
+
+                                var winner;
+
+                                if (userChoice === "rock" && computerChoice === "rock") {
+                                        winner = "tied";
+                                }
+                                else if (userChoice === "paper" && computerChoice === "rock") {
+                                        winner = "user";
+                                }
+                                else if (userChoice === "scissors" && computerChoice === "rock") {
+                                        winner = "computer";
+                                }
+                                else if (userChoice === "rock" && computerChoice === "paper") {
+                                        winner = "computer";
+                                }
+                                else if (userChoice === "paper" && computerChoice === "paper") {
+                                        winner = "tied";
+                                }
+                                else if (userChoice === "scissors" && computerChoice === "paper") {
+                                        winner = "user";
+                                }
+                                else if (userChoice === "rock" && computerChoice === "scissors") {
+                                        winner = "user";
+                                }
+                                else if (userChoice === "paper" && computerChoice === "scissors") {
+                                        winner = "computer";
+                                }
+                                else if (userChoice === "scissors" && computerChoice === "scissors") {
+                                        winner = "tied";
+                                }
+                                alert(winner);
+
+
+                        });
 });
-
-
